ce00234d0cb907c2ae0c58b62444607a4249643a
samGMIT <g00292346@gmit.ie>
2016-07-05 13:53:24 +0100

    adding food added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,11 +1,23 @@
 angular.module('starter.controllers', [])
 
-.controller('homeCtrl', function($scope) {})
-
-.controller('foodCtrl', function($scope) {
-
+.controller('homeCtrl', function($scope, Foods) {
+	$scope.details = {
+		name: "",
+		calories: null
+	}
+	
+	
+	$scope.addFood = function(){
+		Foods.addFood($scope.details.name, $scope.details.calories)
+		$scope.details.name = "";
+		$scope.details.calories = null;
+	}
 })
 
-.controller('calculatorCtrl', function($scope) {
+.controller('foodCtrl', function($scope, Foods) {
+	$scope.food = Foods.all();
+})
+
+.controller('calculatorCtrl', function($scope, Foods) {
 
 });
